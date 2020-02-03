616065e89854913831ba8d25e1a764770dc5a7d8
Tom de Geus <tom@geus.me>
2020-02-03 09:52:40 +0100

    Minor update

--- include/GMatElastoPlasticFiniteStrainSimo/Cartesian3d_eigvals.hpp
+++ include/GMatElastoPlasticFiniteStrainSimo/Cartesian3d_eigvals.hpp
@@ -1,3 +1,22 @@
+/*
+
+(c - MIT) T.W.J. de Geus (Tom) | www.geus.me | github.com/tdegeus/GMatElastoPlasticFiniteStrainSimo
+
+*/
+
+#ifndef GMATELASTOPLASTICFINITESTRAINSIMO_CARTESIAN3D_EIGVALS_HPP
+#define GMATELASTOPLASTICFINITESTRAINSIMO_CARTESIAN3D_EIGVALS_HPP
+
+#include <stdio.h>
+#include <math.h>
+#include "Cartesian3d.h"
+
+// Macros
+#define SQR(x) ((x)*(x)) // x^2
+
+namespace GMatElastoPlasticFiniteStrainSimo {
+namespace Cartesian3d {
+
 // ----------------------------------------------------------------------------
 // Numerical diagonalization of 3x3 matrices
 // Copyright (C) 2006  Joachim Kopp
@@ -16,19 +35,6 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 // ----------------------------------------------------------------------------
-
-#ifndef GMATELASTOPLASTICFINITESTRAINSIMO_CARTESIAN3D_EIGVALS_HPP
-#define GMATELASTOPLASTICFINITESTRAINSIMO_CARTESIAN3D_EIGVALS_HPP
-
-#include <stdio.h>
-#include <math.h>
-#include "Cartesian3d.h"
-
-// Macros
-#define SQR(x) ((x)*(x)) // x^2
-
-namespace GMatElastoPlasticFiniteStrainSimo {
-namespace Cartesian3d {
 
 // ----------------------------------------------------------------------------
 inline int dsyevj3(double A[3][3], double Q[3][3], double w[3])
@@ -49,111 +55,111 @@ inline int dsyevj3(double A[3][3], double Q[3][3], double w[3])
 //  -1: Error (no convergence)
 // ----------------------------------------------------------------------------
 {
-  const int n = 3;
-  double sd, so;                  // Sums of diagonal resp. off-diagonal elements
-  double s, c, t;                 // sin(phi), cos(phi), tan(phi) and temporary storage
-  double g, h, z, theta;          // More temporary storage
-  double thresh;
+    const int n = 3;
+    double sd, so;          // Sums of diagonal resp. off-diagonal elements
+    double s, c, t;         // sin(phi), cos(phi), tan(phi) and temporary storage
+    double g, h, z, theta;  // More temporary storage
+    double thresh;
 
-  // Initialize Q to the identity matrix
-  for (int i=0; i < n; i++)
-  {
-    Q[i][i] = 1.0;
-    for (int j=0; j < i; j++)
-      Q[i][j] = Q[j][i] = 0.0;
-  }
+    // Initialize Q to the identity matrix
+    for (int i=0; i < n; i++)
+    {
+        Q[i][i] = 1.0;
+        for (int j=0; j < i; j++)
+            Q[i][j] = Q[j][i] = 0.0;
+    }
 
-  // Initialize w to diag(A)
-  for (int i=0; i < n; i++)
-    w[i] = A[i][i];
+    // Initialize w to diag(A)
+    for (int i=0; i < n; i++)
+        w[i] = A[i][i];
 
-  // Calculate SQR(tr(A))
-  sd = 0.0;
-  for (int i=0; i < n; i++)
-    sd += fabs(w[i]);
-  sd = SQR(sd);
+    // Calculate SQR(tr(A))
+    sd = 0.0;
+    for (int i=0; i < n; i++)
+        sd += fabs(w[i]);
+    sd = SQR(sd);
 
-  // Main iteration loop
-  for (int nIter=0; nIter < 50; nIter++)
-  {
-    // Test for convergence
-    so = 0.0;
-    for (int p=0; p < n; p++)
-      for (int q=p+1; q < n; q++)
-        so += fabs(A[p][q]);
-    if (so == 0.0)
-      return 0;
+    // Main iteration loop
+    for (int nIter=0; nIter < 50; nIter++)
+    {
+        // Test for convergence
+        so = 0.0;
+        for (int p=0; p < n; p++)
+            for (int q=p+1; q < n; q++)
+                so += fabs(A[p][q]);
+        if (so == 0.0)
+          return 0;
 
-    if (nIter < 4)
-      thresh = 0.2 * so / SQR(n);
-    else
-      thresh = 0.0;
+        if (nIter < 4)
+            thresh = 0.2 * so / SQR(n);
+        else
+            thresh = 0.0;
 
-    // Do sweep
-    for (int p=0; p < n; p++)
-      for (int q=p+1; q < n; q++)
-      {
-        g = 100.0 * fabs(A[p][q]);
-        if (nIter > 4  &&  fabs(w[p]) + g == fabs(w[p])
-                       &&  fabs(w[q]) + g == fabs(w[q]))
-        {
-          A[p][q] = 0.0;
-        }
-        else if (fabs(A[p][q]) > thresh)
-        {
-          // Calculate Jacobi transformation
-          h = w[q] - w[p];
-          if (fabs(h) + g == fabs(h))
-          {
-            t = A[p][q] / h;
-          }
-          else
-          {
-            theta = 0.5 * h / A[p][q];
-            if (theta < 0.0)
-              t = -1.0 / (sqrt(1.0 + SQR(theta)) - theta);
-            else
-              t = 1.0 / (sqrt(1.0 + SQR(theta)) + theta);
-          }
-          c = 1.0/sqrt(1.0 + SQR(t));
-          s = t * c;
-          z = t * A[p][q];
+        // Do sweep
+        for (int p=0; p < n; p++)
+            for (int q=p+1; q < n; q++)
+            {
+                g = 100.0 * fabs(A[p][q]);
+                if (nIter > 4  &&  fabs(w[p]) + g == fabs(w[p])
+                               &&  fabs(w[q]) + g == fabs(w[q]))
+                {
+                    A[p][q] = 0.0;
+                }
+                else if (fabs(A[p][q]) > thresh)
+                {
+                    // Calculate Jacobi transformation
+                    h = w[q] - w[p];
+                    if (fabs(h) + g == fabs(h))
+                    {
+                        t = A[p][q] / h;
+                    }
+                    else
+                    {
+                        theta = 0.5 * h / A[p][q];
+                        if (theta < 0.0)
+                            t = -1.0 / (sqrt(1.0 + SQR(theta)) - theta);
+                        else
+                            t = 1.0 / (sqrt(1.0 + SQR(theta)) + theta);
+                    }
+                    c = 1.0/sqrt(1.0 + SQR(t));
+                    s = t * c;
+                    z = t * A[p][q];
 
-          // Apply Jacobi transformation
-          A[p][q] = 0.0;
-          w[p] -= z;
-          w[q] += z;
-          for (int r=0; r < p; r++)
-          {
-            t = A[r][p];
-            A[r][p] = c*t - s*A[r][q];
-            A[r][q] = s*t + c*A[r][q];
-          }
-          for (int r=p+1; r < q; r++)
-          {
-            t = A[p][r];
-            A[p][r] = c*t - s*A[r][q];
-            A[r][q] = s*t + c*A[r][q];
-          }
-          for (int r=q+1; r < n; r++)
-          {
-            t = A[p][r];
-            A[p][r] = c*t - s*A[q][r];
-            A[q][r] = s*t + c*A[q][r];
-          }
+                    // Apply Jacobi transformation
+                    A[p][q] = 0.0;
+                    w[p] -= z;
+                    w[q] += z;
+                    for (int r=0; r < p; r++)
+                    {
+                        t = A[r][p];
+                        A[r][p] = c*t - s*A[r][q];
+                        A[r][q] = s*t + c*A[r][q];
+                    }
+                    for (int r=p+1; r < q; r++)
+                    {
+                        t = A[p][r];
+                        A[p][r] = c*t - s*A[r][q];
+                        A[r][q] = s*t + c*A[r][q];
+                    }
+                    for (int r=q+1; r < n; r++)
+                    {
+                        t = A[p][r];
+                        A[p][r] = c*t - s*A[q][r];
+                        A[q][r] = s*t + c*A[q][r];
+                    }
 
-          // Update eigenvectors
-          for (int r=0; r < n; r++)
-          {
-            t = Q[r][p];
-            Q[r][p] = c*t - s*Q[r][q];
-            Q[r][q] = s*t + c*Q[r][q];
-          }
-        }
-      }
-  }
+                    // Update eigenvectors
+                    for (int r=0; r < n; r++)
+                    {
+                        t = Q[r][p];
+                        Q[r][p] = c*t - s*Q[r][q];
+                        Q[r][q] = s*t + c*Q[r][q];
+                    }
+                }
+            }
+    }
 
-  return -1;
+    return -1;
 }
 // ----------------------------------------------------------------------------
 
